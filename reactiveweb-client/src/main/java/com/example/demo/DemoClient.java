1c1d4a0c8a9a1a3cc66fd0ecd9c9e67d7ec02966
Arjun Surendra <asurendra@vmware.com>
2018-05-15 16:11:29 +0530

    fix

--- reactiveweb-client/src/main/java/com/example/demo/DemoClient.java
+++ reactiveweb-client/src/main/java/com/example/demo/DemoClient.java
@@ -6,7 +6,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+// import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
@@ -23,12 +23,13 @@ class Main implements ApplicationRunner {
 
   @Autowired
   WebClient webClient() {
-    return WebClient.builder().filter(ExchangeFilterFunctions.basicAuthentication("admin", "password")).build();
+    //return WebClient.builder().filter(ExchangeFilterFunctions.basicAuthentication("admin", "password")).build();
+    return WebClient.builder().build();
   }
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    String response = webClient().get().uri("http://localhost:8080/tweets").retrieve().bodyToMono(String.class).block();
+    String response = webClient().get().uri("http://localhost:8080/api/customers").retrieve().bodyToMono(String.class).block();
     System.out.println("Response: " + response);
   }
 
